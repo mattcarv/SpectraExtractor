b2eced6b9e80f44415df4ec7aa0aaa8e4498ec0f
Matheus Carvalho <113134149+mattcarv@users.noreply.github.com>
2023-12-04 15:52:55 -0500

    Update DisplayExample.py

--- DisplayExample.py
+++ DisplayExample.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 from astropy.wcs import WCS
 from astropy.wcs.utils import proj_plane_pixel_scales
 from astropy import units as u
+from PIL import Image
 from scipy import ndimage
 
-hdul2 = fits.open('/home/mdocarm/Downloads/PROJECTUGC2885-2022/montage/UGC2885FILES-part1/f606_rotated.fits')
-data2 = hdul2[0].data
+hdul2 = fits.open('/home/mdocarm/Downloads/f475_mosaic.fits')
+image = Image.open('HST_rgb.png')
+# data2 = hdul2[0].data
 wcs = WCS(hdul2[0].header)
 plt.rcParams.update({'font.size': 18})
 plt.rcParams["figure.figsize"] = (15,12)
 
-# data2 = ndimage.rotate(data2, 44.97, reshape=False)
+data2 = ndimage.rotate(image, 44.97, reshape=False)
 
 pixel_scale = proj_plane_pixel_scales(wcs)[0] * u.deg / u.pixel
 arcmin = 1 * u.arcmin.to(u.deg)
@@ -20,11 +22,12 @@ arcmin_pixel = (arcmin / pixel_scale).value
 fig, ax = plt.subplots(subplot_kw={'projection': wcs})
 im1 = plt.imshow(data2, origin='lower', cmap='Greys_r', vmin=0.0001, vmax=0.125)
 
-ax.plot([6, 6 + arcmin_pixel], [6, 6], color='black', lw=2)
-ax.text(6 + arcmin_pixel / 2, 8, '1 arcmin', color='black',
+plt.plot([250, 250 + arcmin_pixel], [250, 250], color='white', lw=2)
+plt.text(250 + arcmin_pixel / 2, 275, '1 arcmin', color='white',
           ha='center', va='bottom', fontsize=16)
 ax.coords['ra'].set_axislabel('Right Ascension (J2000)')
 ax.coords['dec'].set_axislabel('Declination (J2000)')
+ax.set_facecolor('k')
 
 # hdu = fits.PrimaryHDU(data2, header=hdul2[0].header)
 # hdu.writeto('f606_rotated.fits')
@@ -32,4 +35,5 @@ ax.coords['dec'].set_axislabel('Declination (J2000)')
 # cbar = plt.colorbar(im1)
 # cbar.set_label('Flux')
 # plt.grid(visible=True, axis='both')
+
 plt.show()
