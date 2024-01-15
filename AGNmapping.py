49fa8698beed7dfa326252a91a672468cf7f0aef
Matheus Carvalho <113134149+mattcarv@users.noreply.github.com>
2024-01-15 16:30:11 -0500

    Create AGNmapping.py

---
+++ AGNmapping.py
@@ -0,0 +1,8 @@
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.wcs.utils import proj_plane_pixel_scales
+
