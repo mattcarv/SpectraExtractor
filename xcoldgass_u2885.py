6a2f3c3d0a06f15e503e28025399f73b9c4a797b
Matheus Carvalho <mdocarm2@uwo.ca>
2024-01-22 17:40:13 -0500

    Update xcoldgass_u2885.py

--- xcoldgass_u2885.py
+++ xcoldgass_u2885.py
@@ -7,19 +7,25 @@ plt.rcParams["figure.figsize"] = [10, 8]
 plt.rcParams.update({'font.size': 18})
 
 
-dat = Table.read('/home/mdocarm/Downloads/xCOLDGASS_PubCat.fits')
+#dat = Table.read('/home/mdocarm/Downloads/xCOLDGASS_PubCat.fits')
+dat = Table.read('C:/Users/mathe/Downloads/xcoldgass_hi')
 df = dat.to_pandas()
+
 
 # SELECTION
 
-u2885 = pd.DataFrame({'LOGMSTAR': [11.68], 'LOGSFR_BEST': [0.21], 'LOGTDEP': [11.04]})
+u2885 = pd.DataFrame({'LOGMSTAR': [11.68], 'LOGSFR_BEST': [0.21], 'LOGTDEP_H2': [11.04],
+                      'LOGTDEP_HI': [10.6], 'Z_PP04_O3N2': [9.05], 'Z_PP04_N2': [9.21]})
 
 df = df[df.LOGMH2>0]
 
-tdep = df.LOGMH2-df.LOGSFR_BEST
-df = df.assign(LOGTDEP = tdep)
+tdep_h2 = df.LOGMH2-df.LOGSFR_BEST
+tdep_hi = df.logMH-df.LOGSFR_BEST
+df = df.assign(LOGTDEP_HI = tdep_hi)
+df = df.assign(LOGTDEP_H2 = tdep_h2)
 
 df = pd.concat([df, u2885])
+
 
 def curve_function(x, a, b, c):
     return a * x - b * x**2 + c * x**3
@@ -35,9 +41,37 @@ x_fit = np.linspace(min(x), 11.4, 100)
 y_fit = curve_function(x_fit, *params)
 
 y_dotted = curve_function(x_fit, params[0], params[1], params[2])
+#%%
+plt.rcParams["figure.figsize"] = [10, 8]
+plt.rcParams.update({'font.size': 18})
 
-plt.scatter(x, y, c=filtered_df.LOGTDEP, cmap='cool', vmax=11.5)
+
+fig, ax = plt.subplots()
+plt.scatter(x, y, c=filtered_df.LOGTDEP_H2, cmap='cool', vmax=11)
+plt.text(11.38, 0.3, 'UGC 2885', c='black', 
+         fontsize=12)
+plt.text(11.4, 1.4, 'H$_{2}$', c='k', fontsize=20, bbox=dict(facecolor='none',
+                                                             edgecolor='red'))
+plt.plot(x_fit, y_fit, 'k', linewidth=2)
+plt.plot(x_fit, y_dotted+0.4, 'k-.', linewidth=1)
+plt.plot(x_fit, y_dotted-0.4, 'k-.', linewidth=1)
+plt.ylabel('log SFR ($M_{\odot} \; yr^{-1}$)')
+plt.xlabel('log Stellar Mass ($M_{\odot}$)')
+
+cbar = plt.colorbar(ticks=[7.8,8.8, 9.8, 10.8])
+cbar.set_label('log $t_{dep}\; (yr)$')
+
+# print("Fitted Parameters:")
+# print("a:", params[0])
+# print("b:", params[1])
+# print("c:", params[2])
+plt.show()
+
+
+plt.scatter(x, y, c=filtered_df.LOGTDEP_HI, cmap='winter')
 plt.text(11.38, 0.3, 'UGC 2885', c='black', fontsize=12)
+plt.text(11.4, 1.4, 'HI', c='k', fontsize=20, bbox=dict(facecolor='none',
+                                                             edgecolor='red'))
 plt.plot(x_fit, y_fit, 'k', linewidth=2)
 plt.plot(x_fit, y_dotted+0.4, 'k-.', linewidth=1)
 plt.plot(x_fit, y_dotted-0.4, 'k-.', linewidth=1)
@@ -47,8 +81,45 @@ plt.xlabel('log Stellar Mass ($M_{\odot}$)')
 cbar = plt.colorbar()
 cbar.set_label('log $t_{dep}\; (yr)$')
 
-print("Fitted Parameters:")
-print("a:", params[0])
-print("b:", params[1])
-print("c:", params[2])
+# print("Fitted Parameters:")
+# print("a:", params[0])
+# print("b:", params[1])
+# print("c:", params[2])
 plt.show()
+#%%
+# Plotting against metallicity
+
+plt.scatter(x, y, c=filtered_df.Z_PP04_O3N2, cmap='Reds')
+plt.text(11.38, 0.3, 'UGC 2885', c='black', fontsize=12)
+plt.plot(x_fit, y_fit, 'k', linewidth=2)
+plt.plot(x_fit, y_dotted+0.4, 'k-.', linewidth=1)
+plt.plot(x_fit, y_dotted-0.4, 'k-.', linewidth=1)
+plt.ylabel('log SFR ($M_{\odot} \; yr^{-1}$)')
+plt.xlabel('log Stellar Mass ($M_{\odot}$)')
+
+cbar = plt.colorbar()
+cbar.set_label('12 + $log(O/H)$')
+
+# print("Fitted Parameters:")
+# print("a:", params[0])
+# print("b:", params[1])
+# print("c:", params[2])
+plt.clf()
+
+plt.scatter(x, y, c=filtered_df.Z_PP04_N2, cmap='Reds')
+plt.text(11.38, 0.3, 'UGC 2885', c='black', fontsize=12)
+plt.plot(x_fit, y_fit, 'k', linewidth=2)
+plt.plot(x_fit, y_dotted+0.4, 'k-.', linewidth=1)
+plt.plot(x_fit, y_dotted-0.4, 'k-.', linewidth=1)
+plt.ylabel('log SFR ($M_{\odot} \; yr^{-1}$)')
+plt.xlabel('log Stellar Mass ($M_{\odot}$)')
+
+cbar = plt.colorbar()
+cbar.set_label('12 + $log(O/H)$')
+
+# print("Fitted Parameters:")
+# print("a:", params[0])
+# print("b:", params[1])
+# print("c:", params[2])
+plt.show()
+
